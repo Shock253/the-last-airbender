bb2b6842eabb968091b3d0773eaee0225b33616e
Ethan Hocking <eshocking253@gmail.com>
2020-07-14 11:02:58 -0600

    Update search spec

--- spec/features/search_spec.rb
+++ spec/features/search_spec.rb
@@ -11,14 +11,17 @@ describe "As a user" do
     click_on "Search For Members"
 
     within ".search_results" do
-      expect(page).to have_content("Number of Results: ")
+      expect(page).to have_content("Number of Results: 20")
       within(first(".member")) do
-        expect(page).to have_content
-        expect(page).to have_content
+        expect(page).to have_content('Afiko')
+        expect(page).to have_content('Fire Nation')
+        within('.enemies') do
+          expect(page).to have_content('Aang')
+        end
+        within('.allies') do
+          expect(page).to have_content('Fire Nation')
+        end
       end
     end
-
-
-
   end
 end
